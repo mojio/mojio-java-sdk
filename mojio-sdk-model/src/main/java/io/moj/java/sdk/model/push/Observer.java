f518af7a921a1e8e4befbca9d3fdba3b901493ec
Stephen Kidson <stephenkidson@gmail.com>
2016-04-12 15:13:57 -0700

    Imported mojio-android-sdk:mojio-sdk-push classes into mojio-sdk-model

---
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/push/Observer.java
@@ -0,0 +1,207 @@
+package io.moj.java.sdk.model.push;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Model object for an observer.
+ * Created by skidson on 16-02-16.
+ */
+public class Observer {
+
+    public static final String LOCAL_ID = "_id";
+    public static final String KEY = "Key";
+    public static final String CREATED_ON = "CreatedOn";
+    public static final String LAST_MODIFIED = "LastModified";
+    public static final String EXPIRY_DATE = "ExpiryDate";
+    public static final String NAME = "Name";
+    public static final String SUBJECT = "Subject";
+    public static final String TYPE = "Type";
+    public static final String FIELDS = "Fields";
+    public static final String TRANSPORTS = "Transports";
+    public static final String CONDITIONS = "Conditions";
+
+    // local _id for storage in SQLite databases
+    private Long _id;
+
+    private String Key;
+    private String CreatedOn;
+    private String LastModified;
+    private String ExpiryDate;
+    private String Name;
+    private String Subject;
+    private Type Type;
+    private List<String> Fields;
+    private List<Transport> Transports;
+    private List<Condition> Conditions;
+
+    public Observer() {}
+
+    public Observer(String key) {
+        this.Key = key;
+    }
+
+    public String getCreatedOn() {
+        return CreatedOn;
+    }
+
+    public String getExpiryDate() {
+        return ExpiryDate;
+    }
+
+    public List<String> getFields() {
+        return Fields;
+    }
+
+    public String getKey() {
+        return Key;
+    }
+
+    public String getLastModified() {
+        return LastModified;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getSubject() {
+        return Subject;
+    }
+
+
+    public void setCreatedOn(String createdOn) {
+        CreatedOn = createdOn;
+    }
+
+    public void setExpiryDate(String expiryDate) {
+        ExpiryDate = expiryDate;
+    }
+
+    public void setKey(String key) {
+        Key = key;
+    }
+
+    public void setLastModified(String lastModified) {
+        LastModified = lastModified;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public void setSubject(String subject) {
+        Subject = subject;
+    }
+
+    public void setFields(List<String> fields) {
+        Fields = fields;
+    }
+
+    public Long getLocalId() {
+        return _id;
+    }
+
+    public void setLocalId(Long id) {
+        this._id = id;
+    }
+
+    public Transport getTransport() {
+        if (Transports != null && !Transports.isEmpty())
+            return Transports.get(0);
+        return null;
+    }
+
+    public List<Transport> getTransports() {
+        return Transports;
+    }
+
+    public void setTransport(Transport transport) {
+        if (transport == null) {
+            this.Transports = null;
+            return;
+        }
+
+        if (this.Transports == null) {
+            this.Transports = new ArrayList<>();
+        } else {
+            this.Transports.clear();
+        }
+        this.Transports.add(transport);
+    }
+
+    public void setTransports(List<Transport> transports) {
+        Transports = transports;
+    }
+
+    public List<Condition> getConditions() {
+        return Conditions;
+    }
+
+    public void setConditions(List<Condition> conditions) {
+        this.Conditions = conditions;
+    }
+
+    public Observer.Type getType() {
+        return Type;
+    }
+
+    public void setType(Observer.Type type) {
+        Type = type;
+    }
+
+    @Override
+    public String toString() {
+        return "Observer{" +
+                "_id=" + _id +
+                ", Key='" + Key + '\'' +
+                ", CreatedOn='" + CreatedOn + '\'' +
+                ", LastModified='" + LastModified + '\'' +
+                ", ExpiryDate='" + ExpiryDate + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Subject='" + Subject + '\'' +
+                ", Type=" + Type +
+                ", Fields=" + Fields +
+                ", Transports=" + Transports +
+                ", Conditions=" + Conditions +
+                '}';
+    }
+
+    /**
+     * Enumeration of observer types. These are the types of entities an observer can watch for
+     * changes on.
+     */
+    public enum Type {
+        @SerializedName("mojio")
+        MOJIO("mojio"),
+
+        @SerializedName("vehicle")
+        VEHICLE("vehicle"),
+
+        @SerializedName("user")
+        USER("user");
+
+        private final String key;
+
+        Type(String key) {
+            this.key = key;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public static Type fromKey(String key) {
+            for (Type type : values()) {
+                if (type.getKey().equals(key))
+                    return type;
+            }
+            return null;
+        }
+    }
+
+}
---
+++ mojio-sdk-model/src/test/java/io/moj/java/sdk/model/push/ObserverTest.java
@@ -0,0 +1,152 @@
+package io.moj.java.sdk.model.push;
+
+import com.google.common.collect.Lists;
+import com.google.gson.Gson;
+import io.moj.mobile.android.sdk.test.TestUtils;
+import org.junit.Test;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import static junit.framework.Assert.*;
+
+public class ObserverTest {
+
+    @Test
+    public void testSetTransport() {
+        Observer o = new Observer("key");
+        Transport transport1 = Transport.forAndroid("gcmRegId");
+        Transport transport2 = Transport.forSignalR("clientId", "hubName");
+
+        o.setTransport(transport1);
+        assertNotNull(o.getTransports());
+        assertEquals(1, o.getTransports().size());
+        assertEquals(transport1, o.getTransports().get(0));
+        assertEquals(transport1, o.getTransport());
+
+        o.setTransport(transport2);
+        assertNotNull(o.getTransports());
+        assertEquals(1, o.getTransports().size());
+        assertEquals(transport2, o.getTransports().get(0));
+        assertEquals(transport2, o.getTransport());
+    }
+
+    @Test
+    public void testSetTransports() {
+        Observer o = new Observer("key");
+        Transport transport1 = Transport.forAndroid("gcmRegId");
+        Transport transport2 = Transport.forSignalR("clientId", "hubName");
+
+        o.setTransports(Lists.newArrayList(transport1, transport2));
+        assertNotNull(o.getTransports());
+        assertEquals(2, o.getTransports().size());
+        assertEquals(transport1, o.getTransports().get(0));
+        assertEquals(transport2, o.getTransports().get(1));
+        assertEquals(transport1, o.getTransport());
+
+        o.setTransport(null);
+        assertNull(o.getTransport());
+        assertNull(o.getTransports());
+    }
+
+    @Test
+    public void testTypeFromKey() {
+        for (Observer.Type type : Observer.Type.values()) {
+            Observer.Type typeFromKey = Observer.Type.fromKey(type.getKey());
+            assertEquals(type, typeFromKey);
+        }
+        assertNull(Observer.Type.fromKey("NotARealKey"));
+    }
+
+    @Test
+    public void testSerialization() {
+        Gson gson = new Gson();
+        Condition condition1 = Condition.debounce(1, 2, 3, 4, 5);
+        Condition condition2 = Condition.delay(6, 7, 8, 9);
+        Condition condition3 = Condition.onPropertyChanged("Property");
+        Condition condition4 = Condition.throttle(0, 3, 6, 9);
+
+        List<Condition> conditions = Lists.newArrayList(
+                condition1,
+                condition2,
+                condition3,
+                condition4
+        );
+
+        Observer o = new Observer();
+        o.setTransport(gson.fromJson(TestJson.TRANSPORT, Transport.class));
+        o.setType(Observer.Type.VEHICLE);
+        o.setFields(Lists.newArrayList("field1", "field2"));
+        o.setConditions(conditions);
+        o.setCreatedOn("createdOn");
+        o.setExpiryDate("expiryDate");
+        o.setKey("key");
+        o.setLastModified("lastModified");
+        o.setSubject("subject");
+
+        String json = gson.toJson(o);
+        assertEquals(TestJson.OBSERVER, json);
+    }
+
+    @Test
+    public void testDeserialization() {
+        Gson gson = new Gson();
+        Observer o = gson.fromJson(TestJson.OBSERVER, Observer.class);
+        Transport t = gson.fromJson(TestJson.TRANSPORT, Transport.class);
+        Condition condition1 = Condition.debounce(1, 2, 3, 4, 5);
+        Condition condition2 = Condition.delay(6, 7, 8, 9);
+        Condition condition3 = Condition.onPropertyChanged("Property");
+        Condition condition4 = Condition.throttle(0, 3, 6, 9);
+
+        List<Condition> conditions = Lists.newArrayList(
+                condition1,
+                condition2,
+                condition3,
+                condition4
+        );
+
+        assertNotNull(o);
+        assertEquals(t, o.getTransport());
+        assertNotNull(o.getTransports());
+        assertEquals(1, o.getTransports().size());
+        assertEquals(t, o.getTransports().get(0));
+        assertEquals(Observer.Type.VEHICLE, o.getType());
+        assertEquals(Lists.newArrayList("field1", "field2"), o.getFields());
+        assertEquals(conditions, o.getConditions());
+        assertEquals("createdOn", o.getCreatedOn());
+        assertEquals("expiryDate", o.getExpiryDate());
+        assertEquals("key", o.getKey());
+        assertEquals("lastModified", o.getLastModified());
+        assertEquals("subject", o.getSubject());
+    }
+
+    @Test
+    public void testToString() throws IllegalAccessException {
+        Observer o = new Gson().fromJson(TestJson.OBSERVER, Observer.class);
+        TestUtils.assertToStringContainsAllFields(o);
+    }
+
+    @Test
+    public void testGettersAndSetters() throws IllegalAccessException {
+        Observer o = new Gson().fromJson(TestJson.OBSERVER, Observer.class);
+
+        // TODO on some JVM's Method.invoke() is not working for setTransport()
+        List<Method> methods = TestUtils.getAllMethods(o);
+        Iterator<Method> i = methods.iterator();
+        while (i.hasNext()) {
+            Method method = i.next();
+            if (method.getName().endsWith("Transport"))
+                i.remove();
+        }
+        TestUtils.assertAccess(o, methods);
+
+        o.setTransport(null);
+        assertNull(o.getTransport());
+
+        o.setTransports(new ArrayList<Transport>());
+        assertNull(o.getTransport());
+    }
+
+}
