d3e026e5c831f32273c00caf1972757413f63fe1
Mauro Horie <mkhorie@gmail.com>
2016-05-17 13:29:27 -0700

    Added conversions for speed and acceleration units.

--- mojio-sdk-model/src/main/java/io/moj/java/sdk/model/enums/SpeedUnit.java
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/enums/SpeedUnit.java
@@ -2,6 +2,9 @@ package io.moj.java.sdk.model.enums;
 
 import com.google.gson.annotations.SerializedName;
 
+import io.moj.java.sdk.math.Multiplier;
+import io.moj.java.sdk.math.UnitConverter;
+
 /**
  * Enum for SpeedUnit types.
  * Created by mhorie on 2016-01-14.
@@ -9,19 +12,31 @@ import com.google.gson.annotations.SerializedName;
 public enum SpeedUnit {
 
     @SerializedName("MilesPerHour")
-    MILES_PER_HOUR("MilesPerHour"),
+    MILES_PER_HOUR("MilesPerHour", new float[] { 1f, 1.60934f }),
 
     @SerializedName("KilometersPerHour")
-    KILOMETERS_PER_HOUR("KilometersPerHour");
+    KILOMETERS_PER_HOUR("KilometersPerHour", new float[] { 0.621371f, 1f });
 
     private String key;
+    private float[] conversions;
 
-    SpeedUnit(String key) {
+    SpeedUnit(String key, float[] conversions) {
         this.key = key;
+        this.conversions = conversions;
     }
 
     public String getKey() {
         return key;
+    }
+
+    public UnitConverter to(SpeedUnit unit) {
+        SpeedUnit[] values = values();
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] == unit) {
+                return new Multiplier(conversions[i]);
+            }
+        }
+        throw new IllegalArgumentException("Cannot convert " + this + " to " + unit);
     }
 
     public static SpeedUnit fromKey(String key) {
