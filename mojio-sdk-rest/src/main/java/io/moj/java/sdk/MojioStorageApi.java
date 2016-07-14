bc5bcc72c1e2f2745da04d801625c651505dc278
Mauro Horie <mkhorie@gmail.com>
2016-07-14 15:35:09 -0700

    Made some updates to the storage endpoints.

--- mojio-sdk-rest/src/main/java/io/moj/java/sdk/MojioStorageApi.java
+++ mojio-sdk-rest/src/main/java/io/moj/java/sdk/MojioStorageApi.java
@@ -1,6 +1,8 @@
 package io.moj.java.sdk;
 
 import java.util.Map;
+
+import io.moj.java.sdk.model.response.ListResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -20,20 +22,20 @@ public interface MojioStorageApi {
 
     // region Storage
     @GET("{resource}/{id}/store")
-    Call<String[]> getStorage(@Path("resource") String resource, @Path("id") String id, @QueryMap Map<String, Integer> params);
+    Call<ListResponse<Map<String, String>>> getStorage(@Path("resource") String resource, @Path("id") String id, @QueryMap Map<String, Integer> params);
 
     @DELETE("{resource}/{id}/store/{key}")
-    Call<String[]> deleteStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key);
+    Call<String> deleteStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key);
 
     @GET("{resource}/{id}/store/{key}")
     Call<String> getStorageValue(@Path("resource") String resource, @Path("id") String id, @Path("key") String key);
 
     @POST("{resource}/{id}/store/{key}")
-    Call<String[]> createStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key,
+    Call<String> createStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key,
                                         @Body String storageValue);
 
     @PUT("{resource}/{id}/store/{key}")
-    Call<String[]> updateStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key,
+    Call<String> updateStorage(@Path("resource") String resource, @Path("id") String id, @Path("key") String key,
                                      @Body String storageValue);
     // endregion
 
