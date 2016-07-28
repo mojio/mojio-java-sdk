1e6cf86915af601c1fcfb75fb9cc6dcebbf6b132
Stephen Kidson <stephenkidson@gmail.com>
2016-07-28 14:44:01 -0700

    Removed Enabled field from Geofence

--- mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Geofence.java
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Geofence.java
@@ -13,13 +13,11 @@ public class Geofence extends AbstractMojioObject {
     public static final String NAME = "Name";
     public static final String DESCRIPTION = "Description";
     public static final String REGION = "Region";
-    public static final String ENABLED = "Enabled";
     public static final String TAGS = "Tags";
 
     private String Name;
     private String Description;
     private Region Region;
-    private Boolean Enabled;
     private String[] Tags;
 
     public String getName() {
@@ -36,14 +34,6 @@ public class Geofence extends AbstractMojioObject {
 
     public void setDescription(String description) {
         Description = description;
-    }
-
-    public Boolean getEnabled() {
-        return Enabled;
-    }
-
-    public void setEnabled(Boolean enabled) {
-        Enabled = enabled;
     }
 
     public String[] getTags() {
@@ -68,7 +58,6 @@ public class Geofence extends AbstractMojioObject {
                 "Name='" + Name + '\'' +
                 ", Description='" + Description + '\'' +
                 ", Region=" + Region +
-                ", Enabled=" + Enabled +
                 ", Tags=" + Arrays.toString(Tags) +
                 "} " + super.toString();
     }
