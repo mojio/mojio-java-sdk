05f306af81152f91c67c135b5c8c04cd5365954b
Mauro Horie <mkhorie@gmail.com>
2017-02-16 08:45:42 -0800

    Added Polyline property to Trip object.

--- mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Trip.java
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Trip.java
@@ -5,11 +5,11 @@ import java.util.Arrays;
 import io.moj.java.sdk.model.values.Acceleration;
 import io.moj.java.sdk.model.values.Distance;
 import io.moj.java.sdk.model.values.FuelEfficiency;
-import io.moj.java.sdk.model.values.Location;
-import io.moj.java.sdk.model.values.Speed;
 import io.moj.java.sdk.model.values.FuelLevel;
+import io.moj.java.sdk.model.values.Location;
 import io.moj.java.sdk.model.values.Odometer;
 import io.moj.java.sdk.model.values.Rpm;
+import io.moj.java.sdk.model.values.Speed;
 import io.moj.java.sdk.utils.TimeUtils;
 
 /**
@@ -35,6 +35,7 @@ public class Trip extends AbstractMojioObject {
     public static final String MAX_RPM = "MaxRPM";
     public static final String MAX_ACCELERATION = "MaxAcceleration";
     public static final String MAX_DECELERATION = "MaxDeceleration";
+    public static final String POLYLINE = "Polyline";
     public static final String FUEL_EFFICIENCY = "FuelEfficiency";
     public static final String START_FUEL_LEVEL = "StartFuelLevel";
     public static final String END_FUEL_LEVEL = "EndFuelLevel";
@@ -59,6 +60,7 @@ public class Trip extends AbstractMojioObject {
     private Rpm MaxRPM;
     private Acceleration MaxAcceleration;
     private Acceleration MaxDeceleration;
+    private String Polyline;
     private FuelEfficiency FuelEfficiency;
     private FuelLevel StartFuelLevel;
     private FuelLevel EndFuelLevel;
@@ -170,6 +172,14 @@ public class Trip extends AbstractMojioObject {
         Name = name;
     }
 
+    public String getPolyline() {
+        return Polyline;
+    }
+
+    public void setPolyline(String polyline) {
+        Polyline = polyline;
+    }
+
     public FuelLevel getStartFuelLevel() {
         return StartFuelLevel;
     }
@@ -270,6 +280,7 @@ public class Trip extends AbstractMojioObject {
                 ", MaxRPM=" + MaxRPM +
                 ", MaxAcceleration=" + MaxAcceleration +
                 ", MaxDeceleration=" + MaxDeceleration +
+                ", Polyline='" + Polyline + '\'' +
                 ", FuelEfficiency=" + FuelEfficiency +
                 ", StartFuelLevel=" + StartFuelLevel +
                 ", EndFuelLevel=" + EndFuelLevel +
