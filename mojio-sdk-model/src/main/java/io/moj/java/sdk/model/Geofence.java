589e9273158a58e9673d67d3d33728d877efc4d5
Alex Cheng <alexc@moj.io>
2017-03-03 13:37:27 -0800

    AD-817: Updated the Geofence model according to https://1mojio.atlassian.net/browse/PT-1367
    Followed the Transaction enum example.

--- mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Geofence.java
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/Geofence.java
@@ -1,5 +1,7 @@
 package io.moj.java.sdk.model;
 
+import com.google.gson.annotations.SerializedName;
+
 import io.moj.java.sdk.model.values.Region;
 
 import java.util.Arrays;
@@ -14,11 +16,15 @@ public class Geofence extends AbstractMojioObject {
     public static final String DESCRIPTION = "Description";
     public static final String REGION = "Region";
     public static final String TAGS = "Tags";
+    public static final String VEHICLEIDS = "VehicleIds";
+    public static final String ENABLE_ACTIVITY_STREAM = "EnableActivityStream";
 
     private String Name;
     private String Description;
     private Region Region;
     private String[] Tags;
+    private String[] VehicleIds;
+    private State EnableActivityStream;
 
     public String getName() {
         return Name;
@@ -52,6 +58,22 @@ public class Geofence extends AbstractMojioObject {
         Region = region;
     }
 
+    public String[] getVehicleIds() {
+        return VehicleIds;
+    }
+
+    public void setVehicles(String[] vehicleIds) {
+        VehicleIds = vehicleIds;
+    }
+
+    public State getEnableActivityStream() {
+        return EnableActivityStream;
+    }
+
+    public void setEnableActivityStream(State enableActivityStream) {
+        EnableActivityStream = enableActivityStream;
+    }
+
     @Override
     public String toString() {
         return "Geofence{" +
@@ -59,6 +81,41 @@ public class Geofence extends AbstractMojioObject {
                 ", Description='" + Description + '\'' +
                 ", Region=" + Region +
                 ", Tags=" + Arrays.toString(Tags) +
+                ", VehicleIds=" + Arrays.toString(VehicleIds) +
+                ", EnabledActivityStream=" + EnableActivityStream +
                 "} " + super.toString();
     }
+
+    public enum State {
+        @SerializedName("OnEnter")
+        ON_ENTER("OnEnter"),
+
+        @SerializedName("OnExit")
+        ON_EXIT("OnExit"),
+
+        @SerializedName("Always")
+        ALWAYS("Always"),
+
+        @SerializedName("Never")
+        NEVER("Never");
+
+        private final String key;
+
+        State(String key) {
+            this.key = key;
+        }
+
+        public String getKey() {
+            return key;
+        }
+
+        public static State fromKey(String key) {
+            for (State type : values()) {
+                if (type.getKey().equals(key))
+                    return type;
+            }
+            return null;
+        }
+    }
+
 }
