5f795c4fdc9ab122467d8d849b144b6428ab40d1
henrymojio <henryk@moj.io>
2019-04-01 16:22:36 -0700

    PA-2773 [Driver score] Implement driver scoring integration data type changed - for testing purpose

--- mojio-sdk-rest/src/main/java/io/moj/java/sdk/MojioClient.java
+++ mojio-sdk-rest/src/main/java/io/moj/java/sdk/MojioClient.java
@@ -139,7 +139,7 @@ public class MojioClient {
 
     protected void initAuthenticatedApis(Retrofit.Builder retrofitBuilder) {
         restApi = retrofitBuilder
-                .baseUrl("http://13.77.141.27:44303/")
+                .baseUrl("https://13.77.141.27:44303/")
                 .build()
                 .create(MojioRestApi.class);
         pushApi = retrofitBuilder
