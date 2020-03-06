72c50d702094d1cc0afaa7449e0ec8a369c9f1f9
Oleksii_Borys <Lopata_1>
2020-03-06 16:33:28 +0200

    PA-6193 [Biz Expensing] Integration with platform https://1mojio.atlassian.net/browse/PA-6193 PA-6167 [Biz Expensing] New Timeline Card View + Trip Tags/Text https://1mojio.atlassian.net/browse/PA-6167
    Added trip details to Trip model.

---
+++ mojio-sdk-model/src/main/java/io/moj/java/sdk/model/values/Details.java
@@ -0,0 +1,45 @@
+package io.moj.java.sdk.model.values;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Details {
+    @SerializedName(value = "purpose", alternate = "Purpose")
+    private String Purpose;
+    @SerializedName(value = "notes", alternate = "Notes")
+    private String Notes;
+    @SerializedName(value = "lastUpdatedByUserTimeStamp", alternate = "LastUpdatedByUserTimeStamp")
+    private String LastUpdatedByUserTimeStamp;
+
+    public String getPurpose() {
+        return Purpose;
+    }
+
+    public void setPurpose(String purpose) {
+        this.Purpose = purpose;
+    }
+
+    public String getNotes() {
+        return Notes;
+    }
+
+    public void setNotes(String notes) {
+        this.Notes = notes;
+    }
+
+    public String getLastUpdatedByUserTimeStamp() {
+        return LastUpdatedByUserTimeStamp;
+    }
+
+    public void setLastUpdatedByUserTimeStamp(String lastUpdatedByUserTimeStamp) {
+        this.LastUpdatedByUserTimeStamp = lastUpdatedByUserTimeStamp;
+    }
+
+    @Override
+    public String toString() {
+        return "Details{" +
+                "purpose='" + Purpose + '\'' +
+                ", notes='" + Notes + '\'' +
+                ", lastUpdatedByUserTimeStamp='" + LastUpdatedByUserTimeStamp + '\'' +
+                '}';
+    }
+}
